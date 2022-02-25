a3a1b16b24ef1bb0100dac4784760aaf6517e691
Userplant <woranich@gmail.com>
2022-02-25 16:23:46 +0700

    feature:: add load mock env

--- mock/persister_mongo.go
+++ mock/persister_mongo.go
@@ -1,6 +1,13 @@
 package mock
 
-import "smlcloudplatform/internal/microservice"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"smlcloudplatform/internal/microservice"
+
+	"github.com/joho/godotenv"
+)
 
 type PersisterMongoConfig struct{}
 
@@ -12,6 +19,18 @@ func (*PersisterMongoConfig) DB() string {
 	return "micro_test"
 }
 
+const projectDirName = "smlcloudplatform"
+
 func NewPersisterMongo() microservice.IPersisterMongoConfig {
+	re := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	cwd, _ := os.Getwd()
+	rootPath := re.Find([]byte(cwd))
+
+	err := godotenv.Load(string(rootPath) + `/mock/.env`)
+
+	if err != nil {
+		fmt.Println("Load Env Failed ")
+	}
+
 	return &PersisterMongoConfig{}
 }
