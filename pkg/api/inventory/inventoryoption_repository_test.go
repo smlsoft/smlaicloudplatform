591fbfe281c2122b15baf667235641d74f44d0d6
Userplant <woranich@gmail.com>
2022-03-01 10:42:11 +0700

    feature:: add test inventory option repository

---
+++ pkg/api/inventory/inventoryoption_repository_test.go
@@ -0,0 +1,38 @@
+package inventory_test
+
+import (
+	"smlcloudplatform/internal/microservice"
+	"smlcloudplatform/mock"
+	"smlcloudplatform/pkg/api/inventory"
+	"smlcloudplatform/pkg/models"
+	"testing"
+)
+
+func TestCreateInventoryOption(t *testing.T) {
+	mongoPersisterConfig := mock.NewPersisterMongo()
+	mongoPersister := microservice.NewPersisterMongo(mongoPersisterConfig)
+	repo := inventory.NewInventoryOptionRepository(mongoPersister)
+
+	give := models.InventoryOption{
+		GuidFixed:     "fx01",
+		MerchantId:    "mx01",
+		InventoryId:   "inv01",
+		OptionGroupId: "opts01",
+	}
+
+	_, err := repo.Create(give)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	findDoc, err := repo.FindByGuid("fx01", "mx01")
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(findDoc)
+}
