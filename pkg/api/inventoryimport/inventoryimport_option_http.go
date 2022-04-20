4fb3b322c2967b223fbf8a6a577d2d29b70c3b07
Userplant <woranich@gmail.com>
2022-04-20 11:08:32 +0700

    feat:: add inventory option import

---
+++ pkg/api/inventoryimport/inventoryimport_option_http.go
@@ -0,0 +1,153 @@
+package inventoryimport
+
+import (
+	"encoding/json"
+	"net/http"
+	"smlcloudplatform/internal/microservice"
+	"smlcloudplatform/pkg/models"
+	"strconv"
+)
+
+type IInventoryImporOptionMaintHttp interface {
+	RouteSetup()
+}
+
+type InventoryImporOptionMaintHttp struct {
+	ms  *microservice.Microservice
+	cfg microservice.IConfig
+	svc IInventoryOptionMainImportService
+}
+
+func NewInventoryImporOptionMaintHttp(ms *microservice.Microservice, cfg microservice.IConfig) InventoryImporOptionMaintHttp {
+
+	pst := ms.MongoPersister(cfg.MongoPersisterConfig())
+
+	invImportOptionMainRepo := NewInventoryOptionMainImportRepository(pst)
+	invImportOptionMainService := NewInventoryOptionMainImportService(invImportOptionMainRepo)
+
+	return InventoryImporOptionMaintHttp{
+		ms:  ms,
+		cfg: cfg,
+		svc: invImportOptionMainService,
+	}
+}
+
+func (h *InventoryImporOptionMaintHttp) RouteSetup() {
+
+	h.ms.GET("/optionimport", h.ListInventoryOptionMain)
+	h.ms.POST("/optionimport", h.CreateInventoryOptionMain)
+	h.ms.DELETE("/optionimport", h.DeleteInventoryOptionMain)
+
+}
+
+// Create Inventory Option godoc
+// @Description Create Inventory Option
+// @Tags		Inventory
+// @Param		Option  body      models.InventoryOptionMain  true  "Option"
+// @Accept 		json
+// @Success		200	{object}	models.ApiResponse
+// @Failure		401 {object}	models.ApiResponse
+// @Security     AccessToken
+// @Router /option [post]
+func (h InventoryImporOptionMaintHttp) CreateInventoryOptionMain(ctx microservice.IContext) error {
+	authUsername := ctx.UserInfo().Username
+	shopID := ctx.UserInfo().ShopID
+	input := ctx.ReadInput()
+
+	docReq := []models.InventoryOptionMainImport{}
+	err := json.Unmarshal([]byte(input), &docReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	err = h.svc.CreateInBatch(shopID, authUsername, docReq)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusCreated, models.ApiResponse{
+		Success: true,
+	})
+	return nil
+}
+
+// Delete Option godoc
+// @Description Delete Option
+// @Tags		Inventory
+// @Param		id  path      string  true  "Option ID"
+// @Accept 		json
+// @Success		200	{object}	models.ApiResponse
+// @Failure		401 {object}	models.ApiResponse
+// @Security     AccessToken
+// @Router /option/{id} [delete]
+func (h InventoryImporOptionMaintHttp) DeleteInventoryOptionMain(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+
+	input := ctx.ReadInput()
+
+	docReq := []string{}
+	err := json.Unmarshal([]byte(input), &docReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	err = h.svc.Delete(shopID, docReq)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusCreated, models.ApiResponse{
+		Success: true,
+	})
+
+	return nil
+}
+
+// List Inventory Option godoc
+// @Description List Inventory Option
+// @Tags		Inventory
+// @Param		q		query	string		false  "Search Value"
+// @Param		page	query	integer		false  "Add Category"
+// @Param		limit	query	integer		false  "Add Category"
+// @Accept 		json
+// @Success		200	{object}	models.ApiResponse
+// @Failure		401 {object}	models.ApiResponse
+// @Security     AccessToken
+// @Router /option [get]
+func (h InventoryImporOptionMaintHttp) ListInventoryOptionMain(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+
+	page, err := strconv.Atoi(ctx.QueryParam("page"))
+	if err != nil {
+		page = 1
+	}
+
+	limit, err := strconv.Atoi(ctx.QueryParam("limit"))
+
+	if err != nil {
+		limit = 20
+	}
+	docList, pagination, err := h.svc.ListInventory(shopID, page, limit)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusCreated, models.ApiResponse{
+		Success:    true,
+		Data:       docList,
+		Pagination: pagination,
+	})
+	return nil
+}
---
+++ pkg/api/inventoryimport/inventoryimport_option_service.go
@@ -0,0 +1,75 @@
+package inventoryimport
+
+import (
+	"smlcloudplatform/pkg/models"
+	"smlcloudplatform/pkg/utils"
+	"time"
+
+	paginate "github.com/gobeam/mongo-go-pagination"
+)
+
+type IInventoryOptionMainImportService interface {
+	CreateInBatch(shopID string, authUsername string, options []models.InventoryOptionMainImport) error
+	Delete(shopID string, guidList []string) error
+	ListInventory(shopID string, page int, limit int) ([]models.InventoryOptionMainImportInfo, paginate.PaginationData, error)
+}
+
+type InventoryOptionMainImportService struct {
+	repo IInventoryOptionMainImportRepository
+}
+
+func NewInventoryOptionMainImportService(invImportOptionMainRepository IInventoryOptionMainImportRepository) InventoryOptionMainImportService {
+	return InventoryOptionMainImportService{
+		repo: invImportOptionMainRepository,
+	}
+}
+
+func (svc InventoryOptionMainImportService) CreateInBatch(shopID string, authUsername string, options []models.InventoryOptionMainImport) error {
+
+	tempInvDataList := []models.InventoryOptionMainImportDoc{}
+
+	for _, opt := range options {
+		newGuid := utils.NewGUID()
+
+		invDoc := models.InventoryOptionMainImportDoc{}
+
+		invDoc.GuidFixed = newGuid
+		invDoc.ShopID = shopID
+		invDoc.InventoryOptionMainImport = opt
+
+		invDoc.CreatedBy = authUsername
+		invDoc.CreatedAt = time.Now()
+
+		tempInvDataList = append(tempInvDataList, invDoc)
+	}
+
+	err := svc.repo.CreateInBatch(tempInvDataList)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (svc InventoryOptionMainImportService) Delete(shopID string, guidList []string) error {
+
+	err := svc.repo.DeleteInBatch(shopID, guidList)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc InventoryOptionMainImportService) ListInventory(shopID string, page int, limit int) ([]models.InventoryOptionMainImportInfo, paginate.PaginationData, error) {
+	docList, pagination, err := svc.repo.FindPage(shopID, page, limit)
+
+	if err != nil {
+		return []models.InventoryOptionMainImportInfo{}, pagination, err
+	}
+
+	return docList, pagination, nil
+}
