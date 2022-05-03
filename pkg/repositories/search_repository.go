f852e3fdddbddeb2494045e287ef76522d9b7267
Userplant <woranich@gmail.com>
2022-05-03 17:29:23 +0700

    feat:: add repository operation search

---
+++ pkg/repositories/search_repository.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"smlcloudplatform/internal/microservice"
+	"smlcloudplatform/pkg/models/restaurant"
+
+	mongopagination "github.com/gobeam/mongo-go-pagination"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ISearchRepo interface {
+	restaurant.ShopZoneInfo
+}
+
+type SearchRepository[T ISearchRepo] struct {
+	pst microservice.IPersisterMongo
+}
+
+func NewSearchRepository[T ISearchRepo](pst microservice.IPersisterMongo) SearchRepository[T] {
+	return SearchRepository[T]{
+		pst: pst,
+	}
+}
+
+func (repo SearchRepository[T]) FindPage(shopID string, colNameSearch []string, q string, page int, limit int) ([]T, mongopagination.PaginationData, error) {
+
+	searchFilterList := []interface{}{
+		bson.M{"guidfixed": q},
+	}
+
+	for _, colName := range colNameSearch {
+		searchFilterList = append(searchFilterList, bson.M{colName: bson.M{"$regex": primitive.Regex{
+			Pattern: ".*" + q + ".*",
+			Options: "",
+		}}})
+	}
+
+	docList := []T{}
+	pagination, err := repo.pst.FindPage(&T{}, limit, page, bson.M{
+		"shopid":    shopID,
+		"deletedat": bson.M{"$exists": false},
+		"$or":       searchFilterList,
+	}, &docList)
+
+	if err != nil {
+		return []T{}, mongopagination.PaginationData{}, err
+	}
+
+	return docList, pagination, nil
+}
