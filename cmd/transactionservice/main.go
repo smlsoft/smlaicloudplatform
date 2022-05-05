f8422e8976933d777878a8567c60bbf44261949c
suteetoe <sutee.toe@gmail.com>
2022-05-05 13:27:09 +0700

    Create main.go

---
+++ cmd/transactionservice/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"smlcloudplatform/internal/microservice"
+	"smlcloudplatform/pkg/api/purchase"
+	"smlcloudplatform/pkg/api/saleinvoice"
+)
+
+func main() {
+	cfg := microservice.NewConfig()
+	ms, err := microservice.NewMicroservice(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	cacher := ms.Cacher(cfg.CacherConfig())
+	authService := microservice.NewAuthService(cacher, 24*3)
+	ms.HttpMiddleware(authService.MWFuncWithRedis(cacher))
+
+	purchase := purchase.NewPurchaseHttp(ms, cfg)
+	purchase.RouteSetup()
+
+	saleinvoice := saleinvoice.NewSaleinvoiceHttp(ms, cfg)
+	saleinvoice.RouteSetup()
+	ms.Start()
+}
