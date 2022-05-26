3c78ea2e99279fd556a0c69fb85ae6763b31c73d
Userplant <woranich@gmail.com>
2022-05-26 10:15:49 +0700

    feat:: add validate category model

--- pkg/models/category.go
+++ pkg/models/category.go
@@ -9,15 +9,15 @@ import (
 const categoryCollectionName = "categories"
 
 type Category struct {
-	CategoryGuid string `json:"categoryguid" bson:"categoryguid" gorm:"categoryguid"`
+	CategoryGuid string `json:"categoryguid" bson:"categoryguid" gorm:"categoryguid" validate:"required"`
 	ParentGuid   string `json:"parentguid"  bson:"parentguid"`
-	Name1        string `json:"name1" bson:"name1"`
+	Name1        string `json:"name1" bson:"name1" validate:"required"`
 	Name2        string `json:"name2,omitempty" bson:"name2,omitempty"`
 	Name3        string `json:"name3,omitempty" bson:"name3,omitempty"`
 	Name4        string `json:"name4,omitempty" bson:"name4,omitempty"`
 	Name5        string `json:"name5,omitempty" bson:"name5,omitempty"`
-	Image        string `json:"image" bson:"image"`
-	Order        int8   `json:"order" bson:"order"`
+	Image        string `json:"image" bson:"image,omitempty"`
+	Order        int8   `json:"order" bson:"order,omitempty"`
 }
 
 type CategoryInfo struct {
