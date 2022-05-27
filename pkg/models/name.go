06f87f6935b5bb5880f6e13585787cee78cac3d8
Userplant <woranich@gmail.com>
2022-05-27 13:30:19 +0700

    fix:: fix referrene  field name

--- pkg/models/name.go
+++ pkg/models/name.go
@@ -1,9 +1,9 @@
 package models
 
 type Name struct {
-	Name1 string `json:"name1" bson:"name1" validate:"required"`
-	Name2 string `json:"name2,omitempty" bson:"name2,omitempty"`
-	Name3 string `json:"name3,omitempty" bson:"name3,omitempty"`
-	Name4 string `json:"name4,omitempty" bson:"name4,omitempty"`
-	Name5 string `json:"name5,omitempty" bson:"name5,omitempty"`
+	Name1 *string `json:"name1" bson:"name1" validate:"required"`
+	Name2 *string `json:"name2,omitempty" bson:"name2,omitempty"`
+	Name3 *string `json:"name3,omitempty" bson:"name3,omitempty"`
+	Name4 *string `json:"name4,omitempty" bson:"name4,omitempty"`
+	Name5 *string `json:"name5,omitempty" bson:"name5,omitempty"`
 }
