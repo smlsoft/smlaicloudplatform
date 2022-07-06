911eb5ce69bf5381be74d338aaa6c745f6c335c9
suteetoe <sutee.toe@gmail.com>
2022-07-06 14:31:19 +0700

    fix:: add redis config username, tls

--- internal/microservice/config_cacher.go
+++ internal/microservice/config_cacher.go
@@ -1,6 +1,32 @@
 package microservice
 
+import "time"
+
 type CacherConfig struct{}
+
+// ICacherConfig is cacher configuration interface
+type ICacherConfig interface {
+	Endpoint() string
+	Password() string
+	UserName() string
+	TLS() bool
+	DB() int
+	ConnectionSettings() ICacherConnectionSettings
+}
+
+// ICacherConnectionSettings is connection settings for cacher
+type ICacherConnectionSettings interface {
+	PoolSize() int
+	MinIdleConns() int
+	MaxRetries() int
+	MinRetryBackoff() time.Duration
+	MaxRetryBackoff() time.Duration
+	IdleTimeout() time.Duration
+	IdleCheckFrequency() time.Duration
+	PoolTimeout() time.Duration
+	ReadTimeout() time.Duration
+	WriteTimeout() time.Duration
+}
 
 func NewCacherConfig() *CacherConfig {
 	return &CacherConfig{}
@@ -12,6 +38,19 @@ func (cfg *CacherConfig) Endpoint() string {
 
 func (cfg *CacherConfig) Password() string {
 	return getEnv("REDIS_CACHE_PASSWORD", "")
+}
+
+func (cfg *CacherConfig) UserName() string {
+	return getEnv("REDIS_CACHE_USERNAME", "")
+}
+
+func (cfg *CacherConfig) TLS() bool {
+	tlsEnable := getEnv("REDIS_CACHE_TLS_ENABLE", "")
+
+	if tlsEnable != "" && tlsEnable == "true" {
+		return true
+	}
+	return false
 }
 
 func (cfg *CacherConfig) DB() int {
