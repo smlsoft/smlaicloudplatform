6c81d2d9b640b8de3100dbf7712167a95c64d700
userplant <woranich@gmail.com>
2022-10-25 11:22:25 +0700

    feat:: option add names language

--- pkg/product/option/models/option.go
+++ pkg/product/option/models/option.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"smlcloudplatform/pkg/models"
 	common "smlcloudplatform/pkg/models"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -9,31 +10,25 @@ import (
 const inventoryOptionCollectionName string = "inventoryOptions"
 
 type Option struct {
-	Code           string    `json:"code" bson:"code" gorm:"code;primaryKey"`
-	XOrder         int8      `json:"xorder" bson:"xorder" gorm:"xorder"`
-	Required       bool      `json:"required" bson:"required" gorm:"required,type:bool,default:false"`
-	ChoiceType     int8      `json:"choicetype" bson:"choicetype,omitempty" gorm:"choicetype,omitempty"`
-	MaxSelect      int8      `json:"maxselect" bson:"maxselect,omitempty" gorm:"maxselect,omitempty"`
-	Name1          string    `json:"name1" bson:"name1" gorm:"name1"`
-	Name2          string    `json:"name2,omitempty" bson:"name2,omitempty" gorm:"name2,omitempty"`
-	Name3          string    `json:"name3,omitempty" bson:"name3,omitempty" gorm:"name3,omitempty"`
-	Name4          string    `json:"name4,omitempty" bson:"name4,omitempty" gorm:"name4,omitempty"`
-	Name5          string    `json:"name5,omitempty" bson:"name5,omitempty" gorm:"name5,omitempty"`
-	Choices        *[]Choice `json:"choices" bson:"choices" gorm:"choices;foreignKey:OptCode"`
-	IsStockControl bool      `json:"isstockcontrol" bson:"isstockcontrol" gorm:"isstockcontrol"`
+	Code           string `json:"code" bson:"code" gorm:"code;primaryKey"`
+	XOrder         int8   `json:"xorder" bson:"xorder" gorm:"xorder"`
+	Required       bool   `json:"required" bson:"required" gorm:"required,type:bool,default:false"`
+	ChoiceType     int8   `json:"choicetype" bson:"choicetype,omitempty" gorm:"choicetype,omitempty"`
+	MaxSelect      int8   `json:"maxselect" bson:"maxselect,omitempty" gorm:"maxselect,omitempty"`
+	models.Name    `bson:"inline"`
+	Names          *[]models.NameX `json:"names" bson:"names" validate:"required,min=1,unique=Code,dive"`
+	Choices        *[]Choice       `json:"choices" bson:"choices" gorm:"choices;foreignKey:OptCode"`
+	IsStockControl bool            `json:"isstockcontrol" bson:"isstockcontrol" gorm:"isstockcontrol"`
 
 	OptionDetails []OptionDetail `json:"optiondetails" bson:"optiondetails"`
 }
 
 type OptionDetail struct {
-	OptionDetailCode string         `json:"optiondetailcode" bson:"optiondetailcode"`
-	Name1            string         `json:"name1" bson:"name1" gorm:"name1"`
-	Name2            string         `json:"name2,omitempty" bson:"name2,omitempty" gorm:"name2,omitempty"`
-	Name3            string         `json:"name3,omitempty" bson:"name3,omitempty" gorm:"name3,omitempty"`
-	Name4            string         `json:"name4,omitempty" bson:"name4,omitempty" gorm:"name4,omitempty"`
-	Name5            string         `json:"name5,omitempty" bson:"name5,omitempty" gorm:"name5,omitempty"`
-	Image            string         `json:"image" bson:"image" gorm:"image"`
-	ChoiceDetails    []IncudeChoice `json:"choicedetails" bson:"choicedetails"`
+	OptionDetailCode string `json:"optiondetailcode" bson:"optiondetailcode"`
+	models.Name      `bson:"inline"`
+	Names            *[]models.NameX `json:"names" bson:"names" validate:"required,min=1,unique=Code,dive"`
+	Image            string          `json:"image" bson:"image" gorm:"image"`
+	ChoiceDetails    []IncudeChoice  `json:"choicedetails" bson:"choicedetails"`
 }
 
 type Choice struct {
@@ -44,14 +39,11 @@ type Choice struct {
 	Price       float64 `json:"price" bson:"price" gorm:"price"`
 	Qty         float64 `json:"qty" bson:"qty" gorm:"qty"`
 	QtyMax      float64 `json:"qtymax" bson:"qtymax" gorm:"qtymax"`
-	Name1       string  `json:"name1" bson:"name1" gorm:"name1"`
-	Name2       string  `json:"name2,omitempty" bson:"name2,omitempty" gorm:"name2,omitempty"`
-	Name3       string  `json:"name3,omitempty" bson:"name3,omitempty" gorm:"name3,omitempty"`
-	Name4       string  `json:"name4,omitempty" bson:"name4,omitempty" gorm:"name4,omitempty"`
-	Name5       string  `json:"name5,omitempty" bson:"name5,omitempty" gorm:"name5,omitempty"`
-	ItemUnit    string  `json:"itemunit,omitempty" bson:"itemunit" gorm:"itemunit,omitempty"`
-	Selected    bool    `json:"selected" bson:"selected" gorm:"selected,type:bool,default:false"`
-	Default     bool    `json:"default" bson:"default" gorm:"default,type:bool,default:false"`
+	models.Name `bson:"inline"`
+	Names       *[]models.NameX `json:"names" bson:"names" validate:"required,min=1,unique=Code,dive"`
+	ItemUnit    string          `json:"itemunit,omitempty" bson:"itemunit" gorm:"itemunit,omitempty"`
+	Selected    bool            `json:"selected" bson:"selected" gorm:"selected,type:bool,default:false"`
+	Default     bool            `json:"default" bson:"default" gorm:"default,type:bool,default:false"`
 
 	IncudeOptions []IncudeChoice `json:"choicedetails,omitempty" bson:"choicedetails,omitempty"`
 }
@@ -98,7 +90,7 @@ func (InventoryOption) TableName() string {
 	return "inventoryoptions"
 }
 
-//swagger
+// swagger
 type InventoryOptionPageResponse struct {
 	Success    bool                          `json:"success"`
 	Data       []InventoryOptionMainInfo     `json:"data,omitempty"`
