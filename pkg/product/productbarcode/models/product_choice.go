92d2d248b90f7237e0c45979b21df68041ced1e9
userplant <woranich@gmail.com>
2022-11-01 16:27:13 +0700

    feat:: product barcode add new field

--- pkg/product/productbarcode/models/product_choice.go
+++ pkg/product/productbarcode/models/product_choice.go
@@ -3,10 +3,13 @@ package models
 import "smlcloudplatform/pkg/models"
 
 type ProductChoice struct {
-	RefBarcode  string          `json:"refbarcode" bson:"refbarcode"`
-	RefUnitCode string          `json:"refunitcode" bson:"refunitcode"`
-	Names       *[]models.NameX `json:"names" bson:"names" validate:"required,min=1,unique=Code,dive"`
-	Price       float64         `json:"price" bson:"price"`
-	Qty         float64         `json:"qty" bson:"qty"`
-	QtyMax      float64         `json:"qtymax" bson:"qtymax"`
+	GUID           string          `json:"guid" bson:"guid"`
+	RefBarcode     string          `json:"refbarcode" bson:"refbarcode"`
+	RefProductCode string          `json:"refproductcode" bson:"refproductcode"`
+	RefUnitCode    string          `json:"refunitcode" bson:"refunitcode"`
+	Names          *[]models.NameX `json:"names" bson:"names" validate:"required,min=1,unique=Code,dive"`
+	Price          float64         `json:"price" bson:"price"`
+	Qty            float64         `json:"qty" bson:"qty"`
+	QtyMax         float64         `json:"qtymax" bson:"qtymax"`
+	IsStock        bool            `json:"isstock" bson:"isstock"`
 }
