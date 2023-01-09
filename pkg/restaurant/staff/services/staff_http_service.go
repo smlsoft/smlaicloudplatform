9d5a0b8ebed759c1f13c0d1499094e3d224bfb83
userplant <woranich@gmail.com>
2023-01-09 13:41:32 +0700

    feat:: add service staff in restaurant module

---
+++ pkg/restaurant/staff/services/staff_http_service.go
@@ -0,0 +1,320 @@
+package services
+
+import (
+	"errors"
+	"fmt"
+	mastersync "smlcloudplatform/pkg/mastersync/repositories"
+	common "smlcloudplatform/pkg/models"
+	"smlcloudplatform/pkg/restaurant/staff/models"
+	"smlcloudplatform/pkg/restaurant/staff/repositories"
+	"smlcloudplatform/pkg/services"
+	"smlcloudplatform/pkg/utils"
+	"smlcloudplatform/pkg/utils/importdata"
+	"time"
+
+	mongopagination "github.com/gobeam/mongo-go-pagination"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type IStaffHttpService interface {
+	CreateStaff(shopID string, authUsername string, doc models.Staff) (string, error)
+	UpdateStaff(shopID string, guid string, authUsername string, doc models.Staff) error
+	DeleteStaff(shopID string, guid string, authUsername string) error
+	DeleteStaffByGUIDs(shopID string, authUsername string, GUIDs []string) error
+	InfoStaff(shopID string, guid string) (models.StaffInfo, error)
+	SearchStaff(shopID string, q string, page int, limit int, sort map[string]int) ([]models.StaffInfo, mongopagination.PaginationData, error)
+	SearchStaffStep(shopID string, langCode string, q string, skip int, limit int, sort map[string]int) ([]models.StaffInfo, int, error)
+	SaveInBatch(shopID string, authUsername string, dataList []models.Staff) (common.BulkImport, error)
+
+	GetModuleName() string
+}
+
+type StaffHttpService struct {
+	repo repositories.IStaffRepository
+
+	syncCacheRepo mastersync.IMasterSyncCacheRepository
+	services.ActivityService[models.StaffActivity, models.StaffDeleteActivity]
+}
+
+func NewStaffHttpService(repo repositories.IStaffRepository, syncCacheRepo mastersync.IMasterSyncCacheRepository) *StaffHttpService {
+
+	insSvc := &StaffHttpService{
+		repo:          repo,
+		syncCacheRepo: syncCacheRepo,
+	}
+
+	insSvc.ActivityService = services.NewActivityService[models.StaffActivity, models.StaffDeleteActivity](repo)
+
+	return insSvc
+}
+
+func (svc StaffHttpService) CreateStaff(shopID string, authUsername string, doc models.Staff) (string, error) {
+
+	findDoc, err := svc.repo.FindByDocIndentityGuid(shopID, "code", doc.Code)
+
+	if err != nil {
+		return "", err
+	}
+
+	if findDoc.Code != "" {
+		return "", errors.New("code is exists")
+	}
+
+	newGuidFixed := utils.NewGUID()
+
+	docData := models.StaffDoc{}
+	docData.ShopID = shopID
+	docData.GuidFixed = newGuidFixed
+	docData.Staff = doc
+
+	docData.CreatedBy = authUsername
+	docData.CreatedAt = time.Now()
+
+	_, err = svc.repo.Create(docData)
+
+	if err != nil {
+		return "", err
+	}
+
+	svc.saveMasterSync(shopID)
+
+	return newGuidFixed, nil
+}
+
+func (svc StaffHttpService) UpdateStaff(shopID string, guid string, authUsername string, doc models.Staff) error {
+
+	findDoc, err := svc.repo.FindByGuid(shopID, guid)
+
+	if err != nil {
+		return err
+	}
+
+	if findDoc.ID == primitive.NilObjectID {
+		return errors.New("document not found")
+	}
+
+	findDoc.Staff = doc
+
+	findDoc.UpdatedBy = authUsername
+	findDoc.UpdatedAt = time.Now()
+
+	err = svc.repo.Update(shopID, guid, findDoc)
+
+	if err != nil {
+		return err
+	}
+
+	svc.saveMasterSync(shopID)
+
+	return nil
+}
+
+func (svc StaffHttpService) DeleteStaff(shopID string, guid string, authUsername string) error {
+
+	findDoc, err := svc.repo.FindByGuid(shopID, guid)
+
+	if err != nil {
+		return err
+	}
+
+	if findDoc.ID == primitive.NilObjectID {
+		return errors.New("document not found")
+	}
+
+	err = svc.repo.DeleteByGuidfixed(shopID, guid, authUsername)
+	if err != nil {
+		return err
+	}
+
+	svc.saveMasterSync(shopID)
+
+	return nil
+}
+
+func (svc StaffHttpService) DeleteStaffByGUIDs(shopID string, authUsername string, GUIDs []string) error {
+
+	deleteFilterQuery := map[string]interface{}{
+		"guidfixed": bson.M{"$in": GUIDs},
+	}
+
+	err := svc.repo.Delete(shopID, authUsername, deleteFilterQuery)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc StaffHttpService) InfoStaff(shopID string, guid string) (models.StaffInfo, error) {
+
+	findDoc, err := svc.repo.FindByGuid(shopID, guid)
+
+	if err != nil {
+		return models.StaffInfo{}, err
+	}
+
+	if findDoc.ID == primitive.NilObjectID {
+		return models.StaffInfo{}, errors.New("document not found")
+	}
+
+	return findDoc.StaffInfo, nil
+
+}
+
+func (svc StaffHttpService) SearchStaff(shopID string, q string, page int, limit int, sort map[string]int) ([]models.StaffInfo, mongopagination.PaginationData, error) {
+	searchCols := []string{
+		"guidfixed",
+		"code",
+	}
+
+	docList, pagination, err := svc.repo.FindPageSort(shopID, searchCols, q, page, limit, sort)
+
+	if err != nil {
+		return []models.StaffInfo{}, pagination, err
+	}
+
+	return docList, pagination, nil
+}
+
+func (svc StaffHttpService) SearchStaffStep(shopID string, langCode string, q string, skip int, limit int, sort map[string]int) ([]models.StaffInfo, int, error) {
+	searchCols := []string{
+		"code",
+	}
+
+	projectQuery := map[string]interface{}{}
+
+	docList, total, err := svc.repo.FindLimit(shopID, map[string]interface{}{}, searchCols, q, skip, limit, sort, projectQuery)
+
+	if err != nil {
+		return []models.StaffInfo{}, 0, err
+	}
+
+	return docList, total, nil
+}
+
+func (svc StaffHttpService) SaveInBatch(shopID string, authUsername string, dataList []models.Staff) (common.BulkImport, error) {
+
+	payloadList, payloadDuplicateList := importdata.FilterDuplicate[models.Staff](dataList, svc.getDocIDKey)
+
+	itemCodeGuidList := []string{}
+	for _, doc := range payloadList {
+		itemCodeGuidList = append(itemCodeGuidList, doc.Code)
+	}
+
+	findItemGuid, err := svc.repo.FindInItemGuid(shopID, "code", itemCodeGuidList)
+
+	if err != nil {
+		return common.BulkImport{}, err
+	}
+
+	foundItemGuidList := []string{}
+	for _, doc := range findItemGuid {
+		foundItemGuidList = append(foundItemGuidList, doc.Code)
+	}
+
+	duplicateDataList, createDataList := importdata.PreparePayloadData[models.Staff, models.StaffDoc](
+		shopID,
+		authUsername,
+		foundItemGuidList,
+		payloadList,
+		svc.getDocIDKey,
+		func(shopID string, authUsername string, doc models.Staff) models.StaffDoc {
+			newGuid := utils.NewGUID()
+
+			dataDoc := models.StaffDoc{}
+
+			dataDoc.GuidFixed = newGuid
+			dataDoc.ShopID = shopID
+			dataDoc.Staff = doc
+
+			currentTime := time.Now()
+			dataDoc.CreatedBy = authUsername
+			dataDoc.CreatedAt = currentTime
+			return dataDoc
+		},
+	)
+
+	updateSuccessDataList, updateFailDataList := importdata.UpdateOnDuplicate[models.Staff, models.StaffDoc](
+		shopID,
+		authUsername,
+		duplicateDataList,
+		svc.getDocIDKey,
+		func(shopID string, guid string) (models.StaffDoc, error) {
+			return svc.repo.FindByDocIndentityGuid(shopID, "code", guid)
+		},
+		func(doc models.StaffDoc) bool {
+			return doc.Code != ""
+		},
+		func(shopID string, authUsername string, data models.Staff, doc models.StaffDoc) error {
+
+			doc.Staff = data
+			doc.UpdatedBy = authUsername
+			doc.UpdatedAt = time.Now()
+
+			err = svc.repo.Update(shopID, doc.GuidFixed, doc)
+			if err != nil {
+				return nil
+			}
+			return nil
+		},
+	)
+
+	if len(createDataList) > 0 {
+		err = svc.repo.CreateInBatch(createDataList)
+
+		if err != nil {
+			return common.BulkImport{}, err
+		}
+
+	}
+
+	createDataKey := []string{}
+
+	for _, doc := range createDataList {
+		createDataKey = append(createDataKey, doc.Code)
+	}
+
+	payloadDuplicateDataKey := []string{}
+	for _, doc := range payloadDuplicateList {
+		payloadDuplicateDataKey = append(payloadDuplicateDataKey, doc.Code)
+	}
+
+	updateDataKey := []string{}
+	for _, doc := range updateSuccessDataList {
+
+		updateDataKey = append(updateDataKey, doc.Code)
+	}
+
+	updateFailDataKey := []string{}
+	for _, doc := range updateFailDataList {
+		updateFailDataKey = append(updateFailDataKey, svc.getDocIDKey(doc))
+	}
+
+	svc.saveMasterSync(shopID)
+
+	return common.BulkImport{
+		Created:          createDataKey,
+		Updated:          updateDataKey,
+		UpdateFailed:     updateFailDataKey,
+		PayloadDuplicate: payloadDuplicateDataKey,
+	}, nil
+}
+
+func (svc StaffHttpService) getDocIDKey(doc models.Staff) string {
+	return doc.Code
+}
+
+func (svc StaffHttpService) saveMasterSync(shopID string) {
+	if svc.syncCacheRepo != nil {
+		err := svc.syncCacheRepo.Save(shopID, svc.GetModuleName())
+
+		if err != nil {
+			fmt.Printf("save %s cache error :: %s", svc.GetModuleName(), err.Error())
+		}
+	}
+}
+
+func (svc StaffHttpService) GetModuleName() string {
+	return "staff"
+}
---
+++ pkg/restaurant/staff/staff_http.go
@@ -0,0 +1,350 @@
+package staff
+
+import (
+	"encoding/json"
+	"net/http"
+	"smlcloudplatform/internal/microservice"
+	mastersync "smlcloudplatform/pkg/mastersync/repositories"
+	common "smlcloudplatform/pkg/models"
+	"smlcloudplatform/pkg/restaurant/staff/models"
+	"smlcloudplatform/pkg/restaurant/staff/repositories"
+	"smlcloudplatform/pkg/restaurant/staff/services"
+	"smlcloudplatform/pkg/utils"
+)
+
+type IStaffHttp interface{}
+
+type StaffHttp struct {
+	ms  *microservice.Microservice
+	cfg microservice.IConfig
+	svc services.IStaffHttpService
+}
+
+func NewStaffHttp(ms *microservice.Microservice, cfg microservice.IConfig) StaffHttp {
+	pst := ms.MongoPersister(cfg.MongoPersisterConfig())
+	cache := ms.Cacher(cfg.CacherConfig())
+
+	repo := repositories.NewStaffRepository(pst)
+
+	masterSyncCacheRepo := mastersync.NewMasterSyncCacheRepository(cache)
+	svc := services.NewStaffHttpService(repo, masterSyncCacheRepo)
+
+	return StaffHttp{
+		ms:  ms,
+		cfg: cfg,
+		svc: svc,
+	}
+}
+
+func (h StaffHttp) RouteSetup() {
+
+	h.ms.POST("/restaurant/staff/bulk", h.SaveBulk)
+
+	h.ms.GET("/restaurant/staff", h.SearchStaffPage)
+	h.ms.GET("/restaurant/staff/list", h.SearchStaffLimit)
+	h.ms.POST("/restaurant/staff", h.CreateStaff)
+	h.ms.GET("/restaurant/staff/:id", h.InfoStaff)
+	h.ms.PUT("/restaurant/staff/:id", h.UpdateStaff)
+	h.ms.DELETE("/restaurant/staff/:id", h.DeleteStaff)
+	h.ms.DELETE("/restaurant/staff", h.DeleteStaffByGUIDs)
+}
+
+// Create Staff godoc
+// @Description Create Staff
+// @Tags		Staff
+// @Param		Staff  body      models.Staff  true  "Staff"
+// @Accept 		json
+// @Success		201	{object}	common.ResponseSuccessWithID
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff [post]
+func (h StaffHttp) CreateStaff(ctx microservice.IContext) error {
+	authUsername := ctx.UserInfo().Username
+	shopID := ctx.UserInfo().ShopID
+	input := ctx.ReadInput()
+
+	docReq := &models.Staff{}
+	err := json.Unmarshal([]byte(input), &docReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	if err = ctx.Validate(docReq); err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	idx, err := h.svc.CreateStaff(shopID, authUsername, *docReq)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusCreated, common.ApiResponse{
+		Success: true,
+		ID:      idx,
+	})
+	return nil
+}
+
+// Update Staff godoc
+// @Description Update Staff
+// @Tags		Staff
+// @Param		id  path      string  true  "Staff ID"
+// @Param		Staff  body      models.Staff  true  "Staff"
+// @Accept 		json
+// @Success		201	{object}	common.ResponseSuccessWithID
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff/{id} [put]
+func (h StaffHttp) UpdateStaff(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	authUsername := userInfo.Username
+	shopID := userInfo.ShopID
+
+	id := ctx.Param("id")
+	input := ctx.ReadInput()
+
+	docReq := &models.Staff{}
+	err := json.Unmarshal([]byte(input), &docReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	if err = ctx.Validate(docReq); err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	err = h.svc.UpdateStaff(shopID, id, authUsername, *docReq)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusCreated, common.ApiResponse{
+		Success: true,
+		ID:      id,
+	})
+
+	return nil
+}
+
+// Delete Staff godoc
+// @Description Delete Staff
+// @Tags		Staff
+// @Param		id  path      string  true  "Staff ID"
+// @Accept 		json
+// @Success		200	{object}	common.ResponseSuccessWithID
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff/{id} [delete]
+func (h StaffHttp) DeleteStaff(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+	authUsername := userInfo.Username
+
+	id := ctx.Param("id")
+
+	err := h.svc.DeleteStaff(shopID, id, authUsername)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusOK, common.ApiResponse{
+		Success: true,
+		ID:      id,
+	})
+
+	return nil
+}
+
+// Delete Staff godoc
+// @Description Delete Staff
+// @Tags		Staff
+// @Param		Staff  body      []string  true  "Staff GUIDs"
+// @Accept 		json
+// @Success		200	{object}	common.ResponseSuccessWithID
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff [delete]
+func (h StaffHttp) DeleteStaffByGUIDs(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+	authUsername := userInfo.Username
+
+	input := ctx.ReadInput()
+
+	docReq := []string{}
+	err := json.Unmarshal([]byte(input), &docReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	err = h.svc.DeleteStaffByGUIDs(shopID, authUsername, docReq)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusOK, common.ApiResponse{
+		Success: true,
+	})
+
+	return nil
+}
+
+// Get Staff godoc
+// @Description get struct array by ID
+// @Tags		Staff
+// @Param		id  path      string  true  "Staff ID"
+// @Accept 		json
+// @Success		200	{object}	common.ApiResponse
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff/{id} [get]
+func (h StaffHttp) InfoStaff(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+
+	id := ctx.Param("id")
+
+	h.ms.Logger.Debugf("Get Staff %v", id)
+	doc, err := h.svc.InfoStaff(shopID, id)
+
+	if err != nil {
+		h.ms.Logger.Errorf("Error getting document %v: %v", id, err)
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusOK, common.ApiResponse{
+		Success: true,
+		Data:    doc,
+	})
+	return nil
+}
+
+// List Staff godoc
+// @Description get struct array by ID
+// @Tags		Staff
+// @Param		q		query	string		false  "Search Value"
+// @Param		page	query	integer		false  "Add Category"
+// @Param		limit	query	integer		false  "Add Category"
+// @Accept 		json
+// @Success		200	{array}		common.ApiResponse
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff [get]
+func (h StaffHttp) SearchStaffPage(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+
+	q := ctx.QueryParam("q")
+	page, limit := utils.GetPaginationParam(ctx.QueryParam)
+	sort := utils.GetSortParam(ctx.QueryParam)
+	docList, pagination, err := h.svc.SearchStaff(shopID, q, page, limit, sort)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusOK, common.ApiResponse{
+		Success:    true,
+		Data:       docList,
+		Pagination: pagination,
+	})
+	return nil
+}
+
+// List Staff godoc
+// @Description search limit offset
+// @Tags		Staff
+// @Param		q		query	string		false  "Search Value"
+// @Param		offset	query	integer		false  "offset"
+// @Param		limit	query	integer		false  "limit"
+// @Param		lang	query	string		false  "lang"
+// @Accept 		json
+// @Success		200	{array}		common.ApiResponse
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff/list [get]
+func (h StaffHttp) SearchStaffLimit(ctx microservice.IContext) error {
+	userInfo := ctx.UserInfo()
+	shopID := userInfo.ShopID
+
+	q := ctx.QueryParam("q")
+	offset, limit := utils.GetParamOffsetLimit(ctx.QueryParam)
+	sorts := utils.GetSortParam(ctx.QueryParam)
+
+	lang := ctx.QueryParam("lang")
+
+	docList, total, err := h.svc.SearchStaffStep(shopID, lang, q, offset, limit, sorts)
+
+	if err != nil {
+		ctx.ResponseError(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ctx.Response(http.StatusOK, common.ApiResponse{
+		Success: true,
+		Data:    docList,
+		Total:   total,
+	})
+	return nil
+}
+
+// Create Staff Bulk godoc
+// @Description Create Staff
+// @Tags		Staff
+// @Param		Staff  body      []models.Staff  true  "Staff"
+// @Accept 		json
+// @Success		201	{object}	common.BulkReponse
+// @Failure		401 {object}	common.AuthResponseFailed
+// @Security     AccessToken
+// @Router /restaurant/staff/bulk [post]
+func (h StaffHttp) SaveBulk(ctx microservice.IContext) error {
+
+	userInfo := ctx.UserInfo()
+	authUsername := userInfo.Username
+	shopID := userInfo.ShopID
+
+	input := ctx.ReadInput()
+
+	dataReq := []models.Staff{}
+	err := json.Unmarshal([]byte(input), &dataReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	bulkResponse, err := h.svc.SaveInBatch(shopID, authUsername, dataReq)
+
+	if err != nil {
+		ctx.ResponseError(400, err.Error())
+		return err
+	}
+
+	ctx.Response(
+		http.StatusCreated,
+		common.BulkReponse{
+			Success:    true,
+			BulkImport: bulkResponse,
+		},
+	)
+
+	return nil
+}
