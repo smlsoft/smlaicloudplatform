7e0ddc9f2702d946b4dd6fb51a8a1cb7d84c1e09
userplant <woranich@gmail.com>
2023-04-20 11:26:31 +0700

    fix: fix type numeric in transacrion

--- pkg/transaction/stockadjustment/models/stockadjustment.go
+++ pkg/transaction/stockadjustment/models/stockadjustment.go
@@ -12,20 +12,20 @@ const stockadjustmentCollectionName = "transactionStockAdjustment"
 type StockAdjustment struct {
 	models.PartitionIdentity `bson:"inline"`
 	Docno                    string         `json:"docno" bson:"docno"`
-	TotalDiscount            int            `json:"totaldiscount" bson:"totaldiscount"`
-	TotalBeforeVat           int            `json:"totalbeforevat" bson:"totalbeforevat"`
+	TotalDiscount            float64        `json:"totaldiscount" bson:"totaldiscount"`
+	TotalBeforeVat           float64        `json:"totalbeforevat" bson:"totalbeforevat"`
 	GuidRef                  string         `json:"guidref" bson:"guidref"`
 	DocDatetime              time.Time      `json:"docdatetime" bson:"docdatetime"`
 	DocRefNo                 string         `json:"docrefno" bson:"docrefno"`
 	DocRefDate               time.Time      `json:"docrefdate" bson:"docrefdate"`
-	DocType                  int            `json:"doctype" bson:"doctype"`
+	DocType                  int8           `json:"doctype" bson:"doctype"`
 	CustName                 []models.NameX `json:"custname" bson:"custname"`
-	TotalExceptVat           int            `json:"totalexceptvat" bson:"totalexceptvat"`
+	TotalExceptVat           float64        `json:"totalexceptvat" bson:"totalexceptvat"`
 	CashierCode              string         `json:"cashiercode" bson:"cashiercode"`
 	Details                  []Detail       `json:"details" bson:"details"`
 	InquiryType              int            `json:"inquirytype" bson:"inquirytype"`
 	DiscountWord             string         `json:"discountword" bson:"discountword"`
-	TotalCost                int            `json:"totalcost" bson:"totalcost"`
+	TotalCost                float64        `json:"totalcost" bson:"totalcost"`
 	TotalVatValue            float64        `json:"totalvatvalue" bson:"totalvatvalue"`
 	TotalAmount              float64        `json:"totalamount" bson:"totalamount"`
 	TaxDocDate               time.Time      `json:"taxdocdate" bson:"taxdocdate"`
@@ -33,24 +33,25 @@ type StockAdjustment struct {
 	PosID                    string         `json:"posid" bson:"posid"`
 	SaleName                 string         `json:"salename" bson:"salename"`
 	MemberCode               string         `json:"membercode" bson:"membercode"`
-	VatRate                  int            `json:"vatrate" bson:"vatrate"`
-	TotalValue               int            `json:"totalvalue" bson:"totalvalue"`
+	VatRate                  float64        `json:"vatrate" bson:"vatrate"`
+	TotalValue               float64        `json:"totalvalue" bson:"totalvalue"`
 	TaxDocNo                 string         `json:"taxdocno" bson:"taxdocno"`
-	DocRefType               int            `json:"docreftype" bson:"docreftype"`
-	VatType                  int            `json:"vattype" bson:"vattype"`
+	DocRefType               int8           `json:"docreftype" bson:"docreftype"`
+	VatType                  int8           `json:"vattype" bson:"vattype"`
 	CustCode                 string         `json:"custcode" bson:"custcode"`
 	TotalAfterVat            float64        `json:"totalaftervat" bson:"totalaftervat"`
 	TransFlag                int            `json:"transflag" bson:"transflag"`
-	Status                   int            `json:"status" bson:"status"`
+	Status                   int8           `json:"status" bson:"status"`
 }
 
 type Detail struct {
-	SumAmount           int            `json:"sumamount" bson:"sumamount"`
+	SumAmount           float64        `json:"sumamount" bson:"sumamount"`
 	LocationNames       []models.NameX `json:"locationnames" bson:"locationnames"`
-	SumAmountExcludeVat int            `json:"sumamountexcludevat" bson:"sumamountexcludevat"`
+	SumAmountExcludeVat float64        `json:"sumamountexcludevat" bson:"sumamountexcludevat"`
 	DivideValue         int            `json:"dividevalue" bson:"dividevalue"`
-	InquiryType         int            `json:"inquirytype" bson:"inquirytype"`
-	Price               int            `json:"price" bson:"price"`
+	StandValue          int            `json:"standvalue" bson:"standvalue"`
+	InquiryType         int8           `json:"inquirytype" bson:"inquirytype"`
+	Price               float64        `json:"price" bson:"price"`
 	Barcode             string         `json:"barcode" bson:"barcode"`
 	UnitCode            string         `json:"unitcode" bson:"unitcode"`
 	ToWhCode            string         `json:"towhcode" bson:"towhcode"`
@@ -58,32 +59,31 @@ type Detail struct {
 	TotalValueVat       float64        `json:"totalvaluevat" bson:"totalvaluevat"`
 	ItemGuid            string         `json:"itemguid" bson:"itemguid"`
 	ShelfCode           string         `json:"shelfcode" bson:"shelfcode"`
-	TotalQty            int            `json:"totalqty" bson:"totalqty"`
-	StandValue          int            `json:"standvalue" bson:"standvalue"`
-	CalcFlag            int            `json:"calcflag" bson:"calcflag"`
-	VatType             int            `json:"vattype" bson:"vattype"`
+	TotalQty            float64        `json:"totalqty" bson:"totalqty"`
+	CalcFlag            int8           `json:"calcflag" bson:"calcflag"`
+	VatType             int8           `json:"vattype" bson:"vattype"`
 	ToWhNames           []models.NameX `json:"towhnames" bson:"towhnames"`
 	ItemName            []models.NameX `json:"itemname" bson:"itemname"`
 	LineNumber          int            `json:"linenumber" bson:"linenumber"`
 	WhNames             []models.NameX `json:"whnames" bson:"whnames"`
-	AverageCost         int            `json:"averagecost" bson:"averagecost"`
-	LastStatus          int            `json:"laststatus" bson:"laststatus"`
-	TaxType             int            `json:"taxtype" bson:"taxtype"`
+	AverageCost         float64        `json:"averagecost" bson:"averagecost"`
+	LastStatus          int8           `json:"laststatus" bson:"laststatus"`
+	TaxType             int8           `json:"taxtype" bson:"taxtype"`
 	ItemCode            string         `json:"itemcode" bson:"itemcode"`
-	IsPos               int            `json:"ispos" bson:"ispos"`
+	IsPos               int8           `json:"ispos" bson:"ispos"`
 	MultiUnit           bool           `json:"multiunit" bson:"multiunit"`
-	PriceExcludeVat     int            `json:"priceexcludevat" bson:"priceexcludevat"`
+	PriceExcludeVat     float64        `json:"priceexcludevat" bson:"priceexcludevat"`
 	LocationCode        string         `json:"locationcode" bson:"locationcode"`
-	ItemType            int            `json:"itemtype" bson:"itemtype"`
+	ItemType            int8           `json:"itemtype" bson:"itemtype"`
 	Remark              string         `json:"remark" bson:"remark"`
-	Qty                 int            `json:"qty" bson:"qty"`
+	Qty                 float64        `json:"qty" bson:"qty"`
 	Discount            string         `json:"discount" bson:"discount"`
 	DocDatetime         time.Time      `json:"docdatetime" bson:"docdatetime"`
 	WhCode              string         `json:"whcode" bson:"whcode"`
 	ToLocationNames     []models.NameX `json:"tolocationnames" bson:"tolocationnames"`
-	DiscountAmount      int            `json:"discountamount" bson:"discountamount"`
+	DiscountAmount      float64        `json:"discountamount" bson:"discountamount"`
 	UnitNames           []models.NameX `json:"unitnames" bson:"unitnames"`
-	SumOfCost           int            `json:"sumofcost" bson:"sumofcost"`
+	SumOfCost           float64        `json:"sumofcost" bson:"sumofcost"`
 }
 
 type StockAdjustmentInfo struct {
