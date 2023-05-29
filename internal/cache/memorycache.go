cc2d32ad8cb23fda57488f255f91490587775458
userplant <woranich@gmail.com>
2023-05-29 13:16:53 +0700

    feat: add memory cache in microservice

---
+++ internal/cache/memorycache.go
@@ -0,0 +1,31 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+type IMemoryCache interface {
+	Set(key string, value interface{}, d time.Duration)
+	Get(key string) (interface{}, bool)
+}
+
+type MemoryCache struct {
+	cache *cache.Cache
+}
+
+func NewMemoryCache() *MemoryCache {
+	cache := cache.New(5*time.Minute, 10*time.Minute)
+	return &MemoryCache{
+		cache: cache,
+	}
+}
+
+func (c *MemoryCache) Set(key string, value interface{}, d time.Duration) {
+	c.cache.Set(key, value, d)
+}
+
+func (c *MemoryCache) Get(key string) (interface{}, bool) {
+	return c.cache.Get(key)
+}
