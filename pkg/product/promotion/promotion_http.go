1b7385656d53a121a0b0fd1d616c4707f30b004a
userplant <woranich@gmail.com>
2023-06-20 15:01:22 +0700

    feat: promotion type add document filter

--- pkg/product/promotion/promotion_http.go
+++ pkg/product/promotion/promotion_http.go
@@ -271,6 +271,7 @@ func (h PromotionHttp) InfoPromotionByCode(ctx microservice.IContext) error {
 // @Description get list step
 // @Tags		Promotion
 // @Param		q		query	string		false  "Search Value"
+// @Param		promotiontype	query	int		false  "promotiontype"
 // @Param		page	query	integer		false  "Page"
 // @Param		limit	query	integer		false  "Limit"
 // @Accept 		json
@@ -311,6 +312,7 @@ func (h PromotionHttp) SearchPromotionPage(ctx microservice.IContext) error {
 // @Description search limit offset
 // @Tags		Promotion
 // @Param		q		query	string		false  "Search Value"
+// @Param		promotiontype	query	int		false  "promotiontype"
 // @Param		offset	query	integer		false  "offset"
 // @Param		limit	query	integer		false  "limit"
 // @Param		lang	query	string		false  "lang"
