cd95d48d70b1224f110ff3d8a3d1e813a2259988
userplant <woranich@gmail.com>
2023-07-04 17:03:45 +0700

    fix: fix search name pos

--- pkg/pos/setting/services/setting_http_service.go
+++ pkg/pos/setting/services/setting_http_service.go
@@ -206,12 +206,6 @@ func (svc SettingHttpService) SearchSettingStep(shopID string, langCode string, 
 
 	selectFields := map[string]interface{}{}
 
-	if langCode != "" {
-		selectFields["names"] = bson.M{"$elemMatch": bson.M{"code": langCode}}
-	} else {
-		selectFields["names"] = 1
-	}
-
 	docList, total, err := svc.repo.FindStep(shopID, map[string]interface{}{}, searchInFields, selectFields, pageableStep)
 
 	if err != nil {
