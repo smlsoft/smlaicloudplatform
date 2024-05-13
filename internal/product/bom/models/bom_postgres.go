2438fd7deae889ebf62cb7fc6ef7aa55ccd6d0bb
fishdulyapath <fishphatuna@gmail.com>
2024-05-13 14:07:27 +0700

    migratebom

--- internal/product/bom/models/bom_postgres.go
+++ internal/product/bom/models/bom_postgres.go
@@ -4,57 +4,82 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
-	"smlcloudplatform/internal/models"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+// JSONB type to handle JSONB columns in PostgreSQL efficiently
+type JSONB struct {
+	Raw json.RawMessage
+}
+
+// Value implements the driver.Valuer interface for JSONB serialization.
+func (j JSONB) Value() (driver.Value, error) {
+	if len(j.Raw) == 0 {
+		return nil, nil
+	}
+	return j.Raw, nil
+}
+
+// Scan implements the sql.Scanner interface for JSONB deserialization.
+func (j *JSONB) Scan(value interface{}) error {
+	if value == nil {
+		j.Raw = nil
+		return nil
+	}
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	j.Raw = append(j.Raw[:0], b...)
+	return nil
+}
+
+// BOMProductBarcodePg holds data for the BOM product barcode
 type BOMProductBarcodePg struct {
-	BarcodeGuidFixed string       `json:"guidfixed" gorm:"column:guidfixed"`
-	Level            int          `json:"level" gorm:"column:level"`
-	Names            models.JSONB `json:"names" gorm:"column:names;type:jsonb"`
-	ItemUnitCode     string       `json:"itemunitcode" gorm:"column:itemunitcode"`
-	ItemUnitNames    models.JSONB `json:"itemunitnames" gorm:"column:itemunitnames;type:jsonb"`
-	Barcode          string       `json:"barcode" gorm:"column:barcode" validate:"required,min=1"`
-	Condition        bool         `json:"condition" gorm:"column:condition"`
-	DivideValue      float64      `json:"dividevalue" gorm:"column:dividevalue"`
-	StandValue       float64      `json:"standvalue" gorm:"column:standvalue"`
-	Qty              float64      `json:"qty" gorm:"column:qty"`
+	BarcodeGuidFixed string  `json:"guidfixed" gorm:"column:guidfixed"`
+	Level            int     `json:"level" gorm:"column:level"`
+	Names            JSONB   `json:"names" gorm:"column:names;type:jsonb"`
+	ItemUnitCode     string  `json:"itemunitcode" gorm:"column:itemunitcode"`
+	ItemUnitNames    JSONB   `json:"itemunitnames" gorm:"column:itemunitnames;type:jsonb"`
+	Barcode          string  `json:"barcode" gorm:"column:barcode" validate:"required,min=1"`
+	Condition        bool    `json:"condition" gorm:"column:condition"`
+	DivideValue      float64 `json:"dividevalue" gorm:"column:dividevalue"`
+	StandValue       float64 `json:"standvalue" gorm:"column:standvalue"`
+	Qty              float64 `json:"qty" gorm:"column:qty"`
 }
 
+// ProductBarcodeBOMViewPG represents a view for product barcode BOMs
 type ProductBarcodeBOMViewPG struct {
-	ShopID            string `json:"shopid" gorm:"column:shopid"`
-	GuidFixed         string `json:"guidfixed" bson:"guidfixed" gorm:"column:guidfixed;primaryKey"`
-	BOMProductBarcode `gorm:"embedded;"`
-	ImageURI          string    `json:"imageuri" gor:"column:imageuri"`
-	BOM               BOMViewPg `json:"bom" gor:"column:bom"`
+	ShopID            string              `json:"shopid" gorm:"column:shopid"`
+	GuidFixed         string              `json:"guidfixed" bson:"guidfixed" gorm:"column:guidfixed;primaryKey"`
+	BOMProductBarcode BOMProductBarcodePg `gorm:"embedded;"`
+	ImageURI          string              `json:"imageuri" gorm:"column:imageuri"`
+	BOM               BOMViewPg           `json:"bom" gorm:"foreignKey:BOMForeignKey;references:BOMReference"` // Update ForeignKey and References
 }
 
+// TableName sets the custom table name for GORM
 func (b *ProductBarcodeBOMViewPG) TableName() string {
 	return "productBarcodeBOMs"
 }
 
+// CompareTo provides comparison for two ProductBarcodeBOMViewPG objects
 func (s *ProductBarcodeBOMViewPG) CompareTo(other *ProductBarcodeBOMViewPG) bool {
-
-	diff := cmp.Diff(s, other,
-		cmpopts.IgnoreFields(ProductBarcodeBOMViewPG{}, "ShopID", "GuidFixed"),
-	)
-
+	diff := cmp.Diff(s, other, cmpopts.IgnoreFields(ProductBarcodeBOMViewPG{}, "ShopID", "GuidFixed"))
 	return diff == ""
 }
 
+// BOMViewPg defines a list of ProductBarcodeBOMViewPG for custom DB operations
 type BOMViewPg []ProductBarcodeBOMViewPG
 
-// Value Marshal
+// Value serializes the BOMViewPg to JSON for database storage
 func (a BOMViewPg) Value() (driver.Value, error) {
-
 	j, err := json.Marshal(a)
 	return j, err
-	//return json.Marshal(a)
 }
 
-// Scan Unmarshal
+// Scan deserializes JSON from the database into BOMViewPg
 func (a *BOMViewPg) Scan(value interface{}) error {
 	b, ok := value.([]byte)
 	if !ok {
