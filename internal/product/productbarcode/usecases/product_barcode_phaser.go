f98f422ecfcf657961821c542bad19849b7b084e
suteetoe <sutee.toe@gmail.com>
2024-06-10 20:30:37 +0700

    fix:: fix product with main barcode on consumer

--- internal/product/productbarcode/usecases/product_barcode_phaser.go
+++ internal/product/productbarcode/usecases/product_barcode_phaser.go
@@ -18,12 +18,31 @@ func (ProductBarcodePhaser) PhaseProductBarcodeDoc(doc *models.ProductBarcodeDoc
 		return nil, err
 	}
 
-	var pgDoc models.ProductBarcodePg
-	err = json.Unmarshal(j, &pgDoc)
+	var productBarcode models.ProductBarcode
+	err = json.Unmarshal(j, &productBarcode)
 	if err != nil {
 		return nil, err
 	}
 
-	pgDoc.UnitCode = doc.ItemUnitCode
-	return &pgDoc, nil
+	var productBarcodePG models.ProductBarcodePg
+	err = json.Unmarshal(j, &productBarcodePG)
+	if err != nil {
+		return nil, err
+	}
+
+	productBarcodePG.UnitCode = doc.ItemUnitCode
+
+	isNormalProductAndHasRefBarcode := productBarcode.ItemType == 0 && productBarcode.RefBarcodes != nil && len(*productBarcode.RefBarcodes) > 0
+
+	if isNormalProductAndHasRefBarcode {
+		productBarcodePG.MainBarcodeRef = (*productBarcode.RefBarcodes)[0].Barcode
+		productBarcodePG.StandValue = (*productBarcode.RefBarcodes)[0].StandValue
+		productBarcodePG.DivideValue = (*productBarcode.RefBarcodes)[0].DivideValue
+	} else {
+		productBarcodePG.MainBarcodeRef = productBarcode.Barcode
+		productBarcodePG.StandValue = 1
+		productBarcodePG.DivideValue = 1
+	}
+
+	return &productBarcodePG, nil
 }
