e444252df5b02042a14cf3186260e7ce4b69ae73
suteetoe <sutee.toe@gmail.com>
2024-09-17 09:57:25 +0700

    feat:: update quotation dr

--- internal/datatransfer/db_transfer.go
+++ internal/datatransfer/db_transfer.go
@@ -30,277 +30,284 @@ func (db *DBTransfer) BeginTransfer(shopID string) {
 	// start transfer shop
 	todo := context.TODO()
 
-	// // shop
-	// shopDataTransfer := NewShopDataTransfer(connection)
-	// err = shopDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// shop
+	shopDataTransfer := NewShopDataTransfer(connection)
+	err = shopDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // shopuser
-	// shopUserDataTransfer := NewShopUserDataTransfer(connection)
-	// err = shopUserDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// shopuser
+	shopUserDataTransfer := NewShopUserDataTransfer(connection)
+	err = shopUserDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // productbarcode
-	// productBarcodeDataTransfer := NewProductBarcodeDataTransfer(connection)
-	// err = productBarcodeDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// shop employee
+	shopEmployeeDataTransfer := NewShopEmployeeDataTransfer(connection)
+	err = shopEmployeeDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // category
-	// productCategoryTransfer := NewProductCategoryDataTransfer(connection)
-	// err = productCategoryTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// productbarcode
+	productBarcodeDataTransfer := NewProductBarcodeDataTransfer(connection)
+	err = productBarcodeDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // kitchen
-	// kitchenDataTransfer := NewRestaurantKitchenDataTransfer(connection)
-	// err = kitchenDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// category
+	productCategoryTransfer := NewProductCategoryDataTransfer(connection)
+	err = productCategoryTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // restaurant setting
-	// restaurantSettingDataTransfer := NewRestaurantSettingDataTransfer(connection)
-	// err = restaurantSettingDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// kitchen
+	kitchenDataTransfer := NewRestaurantKitchenDataTransfer(connection)
+	err = kitchenDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // bank master
-	// bankMasterDataTransfer := NewBankMasterDataTransfer(connection)
-	// err = bankMasterDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// restaurant setting
+	restaurantSettingDataTransfer := NewRestaurantSettingDataTransfer(connection)
+	err = restaurantSettingDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // book bank
-	// bookbankDataTransfer := NewBookBankDataTransfer(connection)
-	// err = bookbankDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// bank master
+	bankMasterDataTransfer := NewBankMasterDataTransfer(connection)
+	err = bankMasterDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // order device
-	// orderDeviceDataTransfer := NewOrderDeviceDataTransfer(connection)
-	// err = orderDeviceDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// book bank
+	bookbankDataTransfer := NewBookBankDataTransfer(connection)
+	err = bookbankDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // order device setting
-	// orderDeviceSettingDataTransfer := NewOrderDeviceSettingDataTransfer(connection)
-	// err = orderDeviceSettingDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// order device
+	orderDeviceDataTransfer := NewOrderDeviceDataTransfer(connection)
+	err = orderDeviceDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // organization branch
-	// organizationBranchDataTransfer := NewOrganizationBranchDataTransfer(connection)
-	// err = organizationBranchDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// order device setting
+	orderDeviceSettingDataTransfer := NewOrderDeviceSettingDataTransfer(connection)
+	err = orderDeviceSettingDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // organization business type
-	// organizationBusinessTypeDataTransfer := NewOrganizationBusinessTypeDataTransfer(connection)
-	// err = organizationBusinessTypeDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// organization branch
+	organizationBranchDataTransfer := NewOrganizationBranchDataTransfer(connection)
+	err = organizationBranchDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // organization department
-	// organizationDepartmentDataTransfer := NewOrganizationDepartmentDataTransfer(connection)
-	// err = organizationDepartmentDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// organization business type
+	organizationBusinessTypeDataTransfer := NewOrganizationBusinessTypeDataTransfer(connection)
+	err = organizationBusinessTypeDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // pos media
-	// posMediaDataTransfer := NewPosMediaDataTransfer(connection)
-	// err = posMediaDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// organization department
+	organizationDepartmentDataTransfer := NewOrganizationDepartmentDataTransfer(connection)
+	err = organizationDepartmentDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // pos setting
-	// posSettingDataTransfer := NewPosSettingDataTransfer(connection)
-	// err = posSettingDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// pos media
+	posMediaDataTransfer := NewPosMediaDataTransfer(connection)
+	err = posMediaDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // product barcode bom
-	// productbarcodeBOMDataTransfer := NewProductbarcodeBOMDataTransfer(connection)
-	// err = productbarcodeBOMDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// pos setting
+	posSettingDataTransfer := NewPosSettingDataTransfer(connection)
+	err = posSettingDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // product group
-	// productGroupDataTransfer := NewProductGroupDataTransfer(connection)
-	// err = productGroupDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// product barcode bom
+	productbarcodeBOMDataTransfer := NewProductbarcodeBOMDataTransfer(connection)
+	err = productbarcodeBOMDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
+
+	// product group
+	productGroupDataTransfer := NewProductGroupDataTransfer(connection)
+	err = productGroupDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
 	// product unit
-	// productUnitDataTransfer := NewProductUnitDataTransfer(connection)
-	// err = productUnitDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	productUnitDataTransfer := NewProductUnitDataTransfer(connection)
+	err = productUnitDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // order type
-	// orderTypeDataTransfer := NewOrderTypeDataTransfer(connection)
-	// err = orderTypeDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// order type
+	orderTypeDataTransfer := NewOrderTypeDataTransfer(connection)
+	err = orderTypeDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
 	// restaurant table
-	// restaurantTableDataTransfer := NewRestaurantTableDataTransfer(connection)
-	// err = restaurantTableDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	restaurantTableDataTransfer := NewRestaurantTableDataTransfer(connection)
+	err = restaurantTableDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // restaurant zone
-	// restaurantZoneDataTransfer := NewRestaurantZoneDataTransfer(connection)
-	// err = restaurantZoneDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// restaurant zone
+	restaurantZoneDataTransfer := NewRestaurantZoneDataTransfer(connection)
+	err = restaurantZoneDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // sale channel
-	// saleChannelDataTransfer := NewSaleChannelDataTransfer(connection)
-	// err = saleChannelDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// sale channel
+	saleChannelDataTransfer := NewSaleChannelDataTransfer(connection)
+	err = saleChannelDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transport channel
-	// transportChannelDataTransfer := NewSaleTransportDataTransfer(connection)
-	// err = transportChannelDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transport channel
+	transportChannelDataTransfer := NewSaleTransportDataTransfer(connection)
+	err = transportChannelDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // slip image
-	// slipImageDataTransfer := NewSlipImageDataTransfer(connection)
-	// err = slipImageDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// slip image
+	slipImageDataTransfer := NewSlipImageDataTransfer(connection)
+	err = slipImageDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // warehouse
-	// warehouseDataTransfer := NewProductWarehouseDataTransfer(connection)
-	// err = warehouseDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// warehouse
+	warehouseDataTransfer := NewProductWarehouseDataTransfer(connection)
+	err = warehouseDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction paid
-	// transactionPaidDataTransfer := NewTransactionPaidDataTransfer(connection)
-	// err = transactionPaidDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction paid
+	transactionPaidDataTransfer := NewTransactionPaidDataTransfer(connection)
+	err = transactionPaidDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction pay
-	// transactionPayDataTransfer := NewTransactionPayDataTransfer(connection)
-	// err = transactionPayDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction pay
+	transactionPayDataTransfer := NewTransactionPayDataTransfer(connection)
+	err = transactionPayDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction purchase
-	// transactionPurchaseDataTransfer := NewTransactionPurchaseDataTransfer(connection)
-	// err = transactionPurchaseDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction purchase
+	transactionPurchaseDataTransfer := NewTransactionPurchaseDataTransfer(connection)
+	err = transactionPurchaseDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction purchase return
-	// purchaseReturnDataTransfer := NewPurchaseReturnDataTransfer(connection)
-	// err = purchaseReturnDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction purchase return
+	purchaseReturnDataTransfer := NewPurchaseReturnDataTransfer(connection)
+	err = purchaseReturnDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction sale
-	// transactionSaleDataTransfer := NewSaleInvoiceDataTransfer(connection)
-	// err = transactionSaleDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction sale
+	transactionSaleDataTransfer := NewSaleInvoiceDataTransfer(connection)
+	err = transactionSaleDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction sale invoice bom price
-	// transactionSaleInvoiceBomPriceDataTransfer := NewSaleInvoiceBomPricesDataTransfer(connection)
-	// err = transactionSaleInvoiceBomPriceDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction sale invoice bom price
+	transactionSaleInvoiceBomPriceDataTransfer := NewSaleInvoiceBomPricesDataTransfer(connection)
+	err = transactionSaleInvoiceBomPriceDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // sale invoice return
-	// saleInvoiceReturnDataTransfer := NewSaleInvoiceReturnDataTransfer(connection)
-	// err = saleInvoiceReturnDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// sale invoice return
+	saleInvoiceReturnDataTransfer := NewSaleInvoiceReturnDataTransfer(connection)
+	err = saleInvoiceReturnDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction stock balance
-	// transactionStockBalanceDataTransfer := NewStockBalanceDataTransfer(connection)
-	// err = transactionStockBalanceDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction stock balance
+	transactionStockBalanceDataTransfer := NewStockBalanceDataTransfer(connection)
+	err = transactionStockBalanceDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction stock balance detail
+	// transaction stock balance detail
 	transactionStockBalanceDetailDataTransfer := NewStockBalanceDetailDataTransfer(connection)
 	err = transactionStockBalanceDetailDataTransfer.StartTransfer(todo, shopID)
 	if err != nil {
 		panic(err)
 	}
 
-	// // transaction stock adjustment
-	// transactionStockAdjustmentDataTransfer := NewStockAdjustmentDataTransfer(connection)
-	// err = transactionStockAdjustmentDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction stock adjustment
+	transactionStockAdjustmentDataTransfer := NewStockAdjustmentDataTransfer(connection)
+	err = transactionStockAdjustmentDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction stock pickup
-	// transactionStockPickupDataTransfer := NewStockPickupProductDataTransfer(connection)
-	// err = transactionStockPickupDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction stock pickup
+	transactionStockPickupDataTransfer := NewStockPickupProductDataTransfer(connection)
+	err = transactionStockPickupDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction stock receive
-	// transactionStockReceiveDataTransfer := NewStockReceiveProductDataTransfer(connection)
-	// err = transactionStockReceiveDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction stock receive
+	transactionStockReceiveDataTransfer := NewStockReceiveProductDataTransfer(connection)
+	err = transactionStockReceiveDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction stock return
-	// transactionStockReturnDataTransfer := NewStockReturnProductDataTransfer(connection)
-	// err = transactionStockReturnDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction stock return
+	transactionStockReturnDataTransfer := NewStockReturnProductDataTransfer(connection)
+	err = transactionStockReturnDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
-	// // transaction stock transfer
-	// transactionStockTransferDataTransfer := NewStockTransferDataTransfer(connection)
-	// err = transactionStockTransferDataTransfer.StartTransfer(todo, shopID)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// transaction stock transfer
+	transactionStockTransferDataTransfer := NewStockTransferDataTransfer(connection)
+	err = transactionStockTransferDataTransfer.StartTransfer(todo, shopID)
+	if err != nil {
+		panic(err)
+	}
 
 }
---
+++ internal/datatransfer/shop_employee_datatransfer.go
@@ -0,0 +1,86 @@
+package datatransfer
+
+import (
+	"context"
+	"smlcloudplatform/internal/repositories"
+	"smlcloudplatform/internal/shop/employee/models"
+	shopEmployeeRepositories "smlcloudplatform/internal/shop/employee/repositories"
+	"smlcloudplatform/pkg/microservice"
+	msModels "smlcloudplatform/pkg/microservice/models"
+
+	"github.com/userplant/mongopagination"
+)
+
+type ShopEmployeeDataTransfer struct {
+	transferConnection IDataTransferConnection
+}
+
+type IShopEmployeeDataTransferRepository interface {
+	FindPage(ctx context.Context, shopID string, searchInFields []string, pageable msModels.Pageable) ([]models.EmployeeDoc, mongopagination.PaginationData, error)
+}
+
+type ShopEmployeeDataTransferRepository struct {
+	pst microservice.IPersisterMongo
+	repositories.SearchRepository[models.EmployeeDoc]
+}
+
+func NewShopEmployeeDataTransferRepository(mongodbPersister microservice.IPersisterMongo) IShopEmployeeDataTransferRepository {
+	repo := &ShopEmployeeDataTransferRepository{
+		pst: mongodbPersister,
+	}
+
+	repo.SearchRepository = repositories.NewSearchRepository[models.EmployeeDoc](mongodbPersister)
+	return repo
+}
+
+func (repo ShopEmployeeDataTransferRepository) FindPage(ctx context.Context, shopID string, searchInFields []string, pageable msModels.Pageable) ([]models.EmployeeDoc, mongopagination.PaginationData, error) {
+
+	results, pagination, err := repo.SearchRepository.FindPage(ctx, shopID, searchInFields, pageable)
+
+	if err != nil {
+		return nil, mongopagination.PaginationData{}, err
+	}
+
+	return results, pagination, nil
+}
+
+func NewShopEmployeeDataTransfer(transferConnection IDataTransferConnection) IDataTransfer {
+
+	return &ShopEmployeeDataTransfer{
+		transferConnection: transferConnection,
+	}
+}
+
+func (pdt *ShopEmployeeDataTransfer) StartTransfer(ctx context.Context, shopID string) error {
+
+	sourceRepository := NewShopEmployeeDataTransferRepository(pdt.transferConnection.GetSourceConnection())
+	targetRepository := shopEmployeeRepositories.NewEmployeeRepository(pdt.transferConnection.GetTargetConnection())
+
+	pageRequest := msModels.Pageable{
+		Limit: 100,
+		Page:  1,
+	}
+
+	for {
+
+		docs, pages, err := sourceRepository.FindPage(ctx, shopID, nil, pageRequest)
+		if err != nil {
+			return err
+		}
+
+		if len(docs) > 0 {
+			err = targetRepository.CreateInBatch(ctx, docs)
+			if err != nil {
+				return err
+			}
+		}
+
+		if pages.TotalPage > int64(pageRequest.Page) {
+			pageRequest.Page++
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
